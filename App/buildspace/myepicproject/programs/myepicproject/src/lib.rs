f9a50f22d0d010673628611233b0b375e56a0000
crustydev <ademola1145@gmail.com>
2022-06-04 00:37:41 +0100

    refactor code and track new variables dog_count and cat_count
    - introduce cat_count and dog_count to BaseAccount
    - rename total_gifs to image_count
    - rename gif_list to images
    - rename gif_link to image_link
    - rename function start_stuff_off to start_stuff
    - rename function add_gif to add_image
    - rename struct ItemStruct to Image
    - set initial value of vector images to an empty vector in start_stuff

--- App/buildspace/myepicproject/programs/myepicproject/src/lib.rs
+++ App/buildspace/myepicproject/programs/myepicproject/src/lib.rs
@@ -5,25 +5,28 @@ declare_id!("5CJC9SLexjdFwJvuTJaSHg25j3bF9bmXkXm2mhAmFeMm");
 #[program]
 pub mod myepicproject {
     use super::*;
-    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result<()> {
+    pub fn start_stuff(ctx: Context<StartStuff>) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
-        base_account.total_gifs = 0;
+        base_account.image_count = 0;
+        base_account.dog_count = 0;
+        base_account.cat_count = 0;
+        base_account.images = Vec::new();
         Ok(())
     }
 
-    // The fucntion now accepts a gif_link param from the user.
-    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result<()> {
+    // The function now accepts a gif_link param from the user.
+    pub fn add_image(ctx: Context<AddImage>, img_link: String) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
 
         // Build the struct.
-        let item = ItemStruct {
-            gif_link: gif_link.to_string(),
+        let item = Image {
+            image_link: img_link.to_string(),
             user_address: *base_account.to_account_info().key,
         };
 
-        // Add it to the gif_list vector.
-        base_account.gif_list.push(item);
-        base_account.total_gifs += 1;
+        // Add it to the images vector.
+        base_account.images.push(item);
+        base_account.image_count += 1;
         Ok(())
     }
     /* pub fn up_vote_dog(ctx: Context<UpdateGif>, index: u64) -> Result<()> {
@@ -53,7 +56,7 @@ pub mod myepicproject {
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff<'info> {
+pub struct StartStuff<'info> {
     #[account(init, payer = user, space = 9000)]
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
@@ -62,21 +65,23 @@ pub struct StartStuffOff<'info> {
 }
 
 #[derive(Accounts)]
-pub struct AddGif<'info> {
+pub struct AddImage<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
 }
 
-// Create a custom struct for us to work with.
+// Create a custom struct to represent image information
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct ItemStruct {
-    pub gif_link: String,
+pub struct Image {
+    pub image_link: String,
     pub user_address: Pubkey,
 }
 
 #[account]
 pub struct BaseAccount {
-    pub total_gifs: u64,
+    pub image_count: u64,
+    pub dog_count: u64,
+    pub cat_count: u64,
     // Attach a Vector of type ItemStruct to the account.
-    pub gif_list: Vec<ItemStruct>,
+    pub images: Vec<Image>,
 }
