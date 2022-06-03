9f5b92d0cddfe080d5cb19538bdf576380f19f81
C <caaarloos300@gmail.com>
2022-06-03 19:12:43 +0200

    updates

--- App/buildspace/frontend/src/testing_comunication.js
+++ App/buildspace/frontend/src/testing_comunication.js
@@ -1,7 +1,9 @@
 /* Generate a python process using nodejs child_process module */
 var spawn = require('child_process').spawn,
-    py_process = spawn('python3', ['son.py']),
+    py_process = spawn('python3', ['detection.py']),
     data = getData(),
+    Data_Result_Dog = 0;
+    Data_Result_Cat = 0;
     dataResult = '';
 
 /* Define what to do on everytime node application receives data from py_process */
@@ -11,7 +13,7 @@ py_process.stdout.on('data', function(data){
 
 /* At the end, show the result from py_process computing (stored in 'dataResult') */
 py_process.stdout.on('end', function(){
-    console.log('Sum result: ', dataResult);
+    console.log(dataResult);
 });
 
 /* Stringify the array before send to py_process */
@@ -21,5 +23,5 @@ py_process.stdin.write(JSON.stringify(data));
 py_process.stdin.end();
 
 function getData() {
-    return [1, 8, 16, 32, 64, 128, 256];
+    return 'https://media.istockphoto.com/photos/group-of-various-breeds-of-cats-sitting-next-to-each-other-looking-at-picture-id1278389859?k=20&m=1278389859&s=612x612&w=0&h=SAC0uiXC8Qc-4FmQTYZYzLs5H7DbErAnq-8EmLOZwJ4=';
 }
