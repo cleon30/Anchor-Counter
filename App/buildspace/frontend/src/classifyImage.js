162053b7af5531b9f63b7c81dbebe657ca7d2ef8
David Tran <davidtranhq@gmail.com>
2022-06-04 15:28:17 -0400

    Use COCOSSD to count instances

--- App/buildspace/frontend/src/classifyImage.js
+++ App/buildspace/frontend/src/classifyImage.js
@@ -1,14 +1,25 @@
-import mobilenet from "@tensorflow-models/mobilenet";
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
 
-const model = await mobilenet.load()
+import cocoSsd from '@tensorflow-models/coco-ssd';
+
+const model = await cocoSsd.load();
 
 /**
- * Classify the object pictured in the image stored in `img`
+ * Count the number of cats and dogs in the image `img`
  * @param {Element} img the <img> tag containing the image to classify
+ * 
+ * @returns {int[]} The number of cats and dogs, where arr[0] is the number
+ *  of dogs and arr[1] is the number of cats
  */
-export default async function classifyImage(img) {
-  const predictions = await model.classify(img)
-  return predictions.reduce((prev, curr) => {
-    return (prev.probability > curr.probability) ? prev : curr;
+export default async function countCatsAndDogs(img) {
+  const predictions = await model.detect(img);
+  const result = [0, 0]
+  predictions.forEach((pred) => {
+    if (pred.class === 'dog')
+      ++result[0];
+    else if (pred.class === 'cat')
+      ++result[1];
   });
+  return result;
 }
